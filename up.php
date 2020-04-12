a9c84948119932d85432b9d1812f52dfe483d375
peng4740 <1640612063@qq.com>
2020-04-12 17:27:30 +0800

    缩短链接长度

--- up.php
+++ up.php
@@ -46,32 +46,24 @@ function upload($url, $content)
     curl_close($ch);
     return $chContents;
 }
-if(isset($_FILES['file']) { 
-  if($_FILES['file']['size'] > 1048576) { //判断文件大小
-    $return['code'] = 500;
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_FILES["pic"]["error"] <= 0) {
+    $filename = date('Y') . '/' . date('m') . '/' . date('d') . '/' . substr(md5(time()) , 0 , 6) . ".png";
+    $url = "https://api.github.com/repos/" . USER . "/" . REPO . "/contents/" . $filename;
+    $tmpName = './tmp' . md5($filename);
+    move_uploaded_file($_FILES['pic']['tmp_name'], $tmpName);
+    $content = base64_encode(file_get_contents($tmpName));
+    $res = json_decode(upload($url, $content), true);
+    unlink($tmpName);
+    if ($res['content']['path'] != "") {
+        $return['code'] = 'success';
+        $return['data']['filename'] = $filename;
+        $return['data']['url'] = 'https://cdn.jsdelivr.net/gh/' . USER . '/' . REPO . '/' . $res['content']['path'];
+    } else {
+        $return['code'] = 500;
+        $return['url'] = null;
+    }
+} else {
+    $return['code'] = 404;
     $return['url'] = null;
-  } 
-  else { 
-	if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_FILES["pic"]["error"] <= 0) {
-		$filename = date('Y') . '/' . date('m') . '/' . date('d') . '/' . substr(md5(time()) , 0 , 6) . ".png";
-		$url = "https://api.github.com/repos/" . USER . "/" . REPO . "/contents/" . $filename;
-		$tmpName = './tmp' . md5($filename);
-		move_uploaded_file($_FILES['pic']['tmp_name'], $tmpName);
-		$content = base64_encode(file_get_contents($tmpName));
-		$res = json_decode(upload($url, $content), true);
-		unlink($tmpName);
-		if ($res['content']['path'] != "") {
-			$return['code'] = 'success';
-			$return['data']['filename'] = $filename;
-			$return['data']['url'] = 'https://cdn.jsdelivr.net/gh/' . USER . '/' . REPO . '/' . $res['content']['path'];
-		} else {
-			$return['code'] = 500;
-			$return['url'] = null;
-		}
-	} else {
-		$return['code'] = 404;
-		$return['url'] = null;
-	}
-	exit(json_encode($return));
-  } 
 }
+exit(json_encode($return));
